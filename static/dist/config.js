9b8f5dbc7161346a889bbc32435bc30242718434
jingzl <jingzl@hotmail.com>
2025-08-17 10:42:55 +0800

    update

--- static/dist/config.js
+++ static/dist/config.js
@@ -2,6 +2,9 @@
 // 部署时可以直接修改这个文件来改变配置，无需重新编译
 window.APP_CONFIG = {
   // 权限验证开关：'1' 开启，'0' 关闭
-  AUTH_ACCESS: "0",
-  ICP_NUMBER: "",
-}; 
+  AUTH_ACCESS: "1",
+  // 备案号
+  ICP_NUMBER: "hello world !",
+  // API 接口地址 
+  //API_BASE_URL: "http://localhost:8081",
+};
